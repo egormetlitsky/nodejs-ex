4a9751bdafd18ac3958309774b27fd578ae01297
egormetlitsky <egor_metlitsky@mail.ru>
2018-05-23 00:18:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,108 +1,94 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+var http = require('http'),
+    net = require('net'),
+    url = require('url'),
+    settings = {
+        port: 8080,
+        username: "mcyahor",
+        password: "passme"
+    };
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
-
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
+var server = http.createServer(function(request, response) {
+    switch (Auth(request)) {
+        case 407:
+            response.writeHead(407, {
+                "Proxy-Authenticate": "Basic realm=\"qwerty\""
+            });
+            return response.end();
+        case 401:
+            response.writeHead(401, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            return response.end("Unauthorized");
     }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
 
-  }
+    console.log("http: ", request.connection.remoteAddress, "\t", request.url);
+
+    var ph = url.parse(request.url);
+
+    var proxyRequest = http.request({
+        port: ph.port,
+        hostname: ph.hostname,
+        method: request.method,
+        path: ph.path,
+        headers: request.headers
+    });
+
+    proxyRequest.on('response', function(proxyResponse) {
+        proxyResponse.on('data', (chunk) => response.write(chunk, 'binary'));
+        proxyResponse.on('end', () => response.end());
+        proxyResponse.on('error', () => response.end());
+        response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+    });
+
+    request.on('data', (chunk) => proxyRequest.write(chunk, 'binary'));
+    request.on('end', () => proxyRequest.end());
+    request.on('error', () => proxyRequest.end());
+
+}).on('connect', function(request, socketRequest, head) {
+    if (Auth(request)) {
+        socketRequest.end(
+            "HTTP/" + request.httpVersion + " 407 Proxy Authentication Required\r\n" +
+            "Proxy-Authenticate: Basic realm=\"qwertys\"\r\n" +
+            "\r\n"
+        );
+        return;
+    }
+
+    console.log("https:", request.connection.remoteAddress, "\t", request.url);
+
+    var ph = url.parse('http://' + request.url);
+
+    var socket = net.connect(ph.port, ph.hostname, function() {
+        socket.write(head);
+        socketRequest.write("HTTP/" + request.httpVersion + " 200 Connection established\r\n\r\n");
+    });
+
+    socket.on('data', (chunk) => socketRequest.write(chunk));
+    socket.on('end', () => socketRequest.end());
+    socket.on('error', function() {
+        socketRequest.write("HTTP/" + request.httpVersion + " 500 Connection error\r\n\r\n");
+        socketRequest.end();
+    });
+
+    socketRequest.on('data', (chunk) => socket.write(chunk));
+    socketRequest.on('end', () => socket.end());
+    socketRequest.on('error', () => socket.end());
+}).listen(settings.port);
+
+function Auth(request) {
+    if (!request.headers["proxy-authorization"]) {
+        console.log("auth: ", request.connection.remoteAddress, "\t", 407);
+        return 407;
+    } else {
+        var base64 = request.headers["proxy-authorization"].replace(/^.+?\s/, ""),
+            auth = new Buffer(base64, 'base64').toString().split(/:/);
+        if (auth[0] !== settings.username || auth[1] !== settings.password) {
+            console.log("auth>", request.connection.remoteAddress, "\t", 401, auth[0], auth[1]);
+            return 401;
+        }
+    }
 }
-var db = null,
-    dbDetails = new Object();
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+process.on("uncaughtException", function(e) { // Игнорирование ошибок
+    console.error("uncaughtException", e.stack);
 });
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
